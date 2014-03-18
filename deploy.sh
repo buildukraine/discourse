93f086aa889b278971726576a0dbdf6d48beeb8d
Administrator <objective.dll@gmail.com>
2014-03-18 20:10:56 +0200

    Deploy for vargant

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+cd ~/discourse
+sudo git pull
+sudo cp -f -R ~/discourse/config/locales/client.uk.yml ~/apps/discourse/htdocs/config/locales/client.uk.yml
+sudo cp -f -R ~/discourse/app/assets/javascripts/discourse/models/ ~/apps/discourse/htdocs/app/assets/javascripts/discourse/models
+sudo cp -f -R ~/discourse/app/assets/stylesheets/ ~/apps/discourse/htdocs/app/assets/stylesheets
+
+
+
+
